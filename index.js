efb2e865bcf3fb55c7a2f0d49d9fa0a3cc211ea8
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-03 09:03:06 -0800

    fix linting errors

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const { isNumber } = require('./lib/types.js');
+const { isString } = require('./lib/types.js');
+const { isBoolean } = require('./lib/types.js');
+const { isArray } = require('./lib/types.js');
+const { isObject } = require('./lib/types.js');
+const { isFunction } = require('./lib/types.js');
 
 console.log(isNumber('3'));
+console.log(isString('hi'));
+console.log(isBoolean(true));
+console.log(isArray([1, 2, 3, 4]));
+console.log(isObject({}));
+console.log(isFunction(() => 1));
